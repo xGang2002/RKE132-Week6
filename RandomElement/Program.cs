2370dafa5033d4a508955333dc42853d63913da0
Sanya <sassik6969@gmail.com>
2023-02-16 20:37:35 +0200

    6th week

---
+++ RandomElement/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] snacks = { "sushi", "pizza", "burger", "chicken wings", "Asian takeaway" };
+
+Random rnd = new Random();
+
+int randomIndex = rnd.Next(0, snacks.Length);
+
+Console.WriteLine($"Tonight we are going to eat {snacks[randomIndex]}.");    
